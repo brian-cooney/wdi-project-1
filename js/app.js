a0e792e5f71afb83c4c7de0ccfaaa63d8f281b81
brian-cooney <bcooney@gmail.com>
2017-04-03 10:43:35 +0100

    Sudo code added

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 /*
 
-WHACK A MOLE STYLE GAME
-  The player has a set amount of time to click as many blocks as possible
+WHACK A MOLE STYLE GAME USING WDI STUDENT PORTRAITS - Version 1
+  Using photos of the WDI class as objects to click on a grid.
+  The player has a set amount of time to click as many objects as possible
   score is recored for each round
+  Speed of game play increases as the rounds progress.
   
 
-SUDO CODE
-  Make gameboard play area
-  Get one div block to turn off with mouse click
-  Make function to randomly display blocks
-  Create countdown timer function to display in game
 
+SUDO CODE
+  Get one div block to turn off with mouse click
+  Randomly display blocks to the screen
+  Add countdown timer to game
+  keep track of the players score
 
 */
 

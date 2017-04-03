4699ce40d6434bccaca1e8e71435ec95f5db99aa
brian-cooney <bcooney@gmail.com>
2017-04-03 10:11:20 +0100

    divs hide when clicked

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log('app.js is linked, great success!');
+// console.log('app.js is linked, great success!');
+//document ready jQuery
+
+//make gameboard play area
+//get 1 div block to turn off with mouse click
+//
+
+$(document).ready(function(){
+
+  $('.boxPark').on('click', function() {
+    $(this).hide(1000);
+
+  });
+
+});

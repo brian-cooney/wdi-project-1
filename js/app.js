f76cbee741b9ffd1b0643f4e504752f44d075d25
brian-cooney <bcooney@gmail.com>
2017-04-03 10:43:14 +0100

    Sudo code added

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
-// console.log('app.js is linked, great success!');
-//document ready jQuery
+/*
 
-//make gameboard play area
-//get 1 div block to turn off with mouse click
-//
+WHACK A MOLE STYLE GAME
+  The player has a set amount of time to click as many blocks as possible
+  score is recored for each round
+  
+
+SUDO CODE
+  Make gameboard play area
+  Get one div block to turn off with mouse click
+  Make function to randomly display blocks
+  Create countdown timer function to display in game
+
+
+*/
 
 $(document).ready(function(){
 
   $('.boxPark').on('click', function() {
     $(this).hide(1000);
+
 
   });
 
